a0dbfe8726fe495ffe7a0d362e6a782bbeae5bbe
Antony Wu <antony@meshway.com>
2015-10-28 22:57:38 -0500

    OMG_Android: Part 1 of Second Android Tutorial
    Learning different layouts ranging from Relative to Linear Layout
    Got some idea between Gravity and Layout_Gravity
    
    Good stuff

--- OMG_Android/app/src/main/java/com/meshway/www/omg_android/MainActivity.java
+++ OMG_Android/app/src/main/java/com/meshway/www/omg_android/MainActivity.java
@@ -8,8 +8,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView mainTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mainTextView = (TextView) findViewById(R.id.main_textview);
+        mainTextView.setText("Set in Java!");
     }
 
     @Override
@@ -33,20 +38,5 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
