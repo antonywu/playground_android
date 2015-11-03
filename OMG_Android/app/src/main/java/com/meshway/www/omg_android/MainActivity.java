1f2cc9079288375374d525e57a343dfc57dde0e1
Antony Wu <antony@meshway.com>
2015-11-02 21:34:55 -0600

    OMG_Android: support for editText and ListItem objects

--- OMG_Android/app/src/main/java/com/meshway/www/omg_android/MainActivity.java
+++ OMG_Android/app/src/main/java/com/meshway/www/omg_android/MainActivity.java
@@ -5,15 +5,27 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        AdapterView.OnItemClickListener {
     TextView mainTextView;
     Button mainButton;
+    EditText mainEditText;
+    ListView mainListView;
+    ArrayAdapter mArrayAdapter;
+    ArrayList mNameList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +48,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mainButton = (Button) findViewById(R.id.main_button);
         mainButton.setOnClickListener(this);
+
+        mainEditText = (EditText) findViewById(R.id.main_edittext);
+
+        mainListView = (ListView) findViewById(R.id.main_listview);
+        mArrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                mNameList);
+        mainListView.setAdapter(mArrayAdapter);
+        mainListView.setOnItemClickListener(this);
     }
 
     @Override
@@ -47,6 +68,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        mainTextView.setText("Button pressed.");
+        mainTextView.setText(mainEditText.getText().toString() + " is learning Android development");
+        mNameList.add(mainEditText.getText().toString());
+        mArrayAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.d("omg android", position + ": " + mNameList.get(position));
     }
 }
