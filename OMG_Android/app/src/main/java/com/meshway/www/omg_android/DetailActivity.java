b03ec544c86a2577bfcb158bd27fd2053d9fb84d
Antony Wu <antony@meshway.com>
2015-11-05 16:38:42 -0600

    OMG_Android: Share book cover to friends

--- OMG_Android/app/src/main/java/com/meshway/www/omg_android/DetailActivity.java
+++ OMG_Android/app/src/main/java/com/meshway/www/omg_android/DetailActivity.java
@@ -1,9 +1,14 @@
 package com.meshway.www.omg_android;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -14,6 +19,7 @@ import com.squareup.picasso.Picasso;
 public class DetailActivity extends AppCompatActivity {
     private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/";
     String mImageURL;
+    ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -31,5 +37,30 @@ public class DetailActivity extends AppCompatActivity {
 
             Picasso.with(this).load(mImageURL).placeholder(R.mipmap.img_books_loading).into(imageView);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        MenuItem shareItem = menu.findItem(R.id.menu_item_share);
+
+        if(null != shareItem) {
+
+            mShareActionProvider = (android.support.v7.widget.ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        }
+
+        setShareIntent();
+        return true;
+    }
+
+    private void setShareIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Book Recommendations");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, mImageURL);
+
+        mShareActionProvider.setShareIntent(shareIntent);
     }
 }
